5070f67809a965d5949ab0241e90fe7368017fb9
Emre Yılmaz <mail@emreyilmaz.me>
2013-10-12 22:45:22 +0300

    fix indent.

--- lama/config.py
+++ lama/config.py
@@ -4,9 +4,10 @@ import ConfigParser
 
 config_file = os.path.expanduser("~/.imgur.conf")
 
+
 def get_client_id():
 
-	parser = ConfigParser.RawConfigParser()
-	parser.read(config_file)
+    parser = ConfigParser.RawConfigParser()
+    parser.read(config_file)
 
-	return parser.get("client", "id")
+    return parser.get("client", "id")
