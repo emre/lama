7693d84fbf1686f3a9a8bdca8268d29e65bc31dc
Emre Yılmaz <mail@emreyilmaz.me>
2013-10-12 22:35:52 +0300

    change name

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
 setup(
-    name='imgui',
+    name='lama',
     version='0.4',
-    packages=['imgui'],
-    url='http://github.com/emre/imgui',
+    packages=['lama'],
+    url='http://github.com/emre/lama',
     license='MIT',
     author='Emre Yilmaz',
     author_email='mail@emreyilmaz.me',
     description='a tool to take screenshots and upload to imgur from command line.',
     install_requires=["requests", ],
     entry_points={
-        'console_scripts': ['imgui = imgui.upload:main',]
+        'console_scripts': ['lama = lama.upload:main',]
     }
 )
