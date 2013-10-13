07e3e6fb9c71a40499df511b3e5ad4979975869c
Emre Yılmaz <mail@emreyilmaz.me>
2013-10-13 10:54:16 +0300

    raise a helpful exception when config file does not exist

--- lama/config.py
+++ lama/config.py
@@ -7,6 +7,9 @@ config_file = os.path.expanduser("~/.imgur.conf")
 
 def get_client_id():
 
+    if not os.path.exists(config_file):
+        raise IOError("you need to create a config file as ~/.imgur.conf")
+
     parser = ConfigParser.RawConfigParser()
     parser.read(config_file)
 
